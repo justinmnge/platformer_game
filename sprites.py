1b2f89b7c49b28761b4d1a1eecaf60f76a88a624
Justin Monge <justin.mnge@gmail.com>
2024-10-11 21:14:57 -0300

    added animations, images and jumping mechanic

--- sprites.py
+++ sprites.py
@@ -5,29 +5,42 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos)
+
+class AnimatedSprite(Sprite):
+    def __init__(self, frames, pos, groups):
+        self.frames, self.frame_index, self.animation_speed = frames, 0, 10
+        super().__init__(pos, self.frames[self.frame_index], groups)
         
-class Player(Sprite):
-    def __init__(self, pos, groups, collision_sprites):
-        surf = pygame.Surface((40, 80))
-        super().__init__(pos, surf, groups)
-        self.image = pygame.image.load(join('images', 'player', '0.png')).convert_alpha()
-        self.rect = self.image.get_frect(center = pos)
+    def animate(self, dt):
+        self.frame_index += self.animation_speed * dt
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
         
-        # movement
+class Player(AnimatedSprite):
+    def __init__(self, pos, groups, collision_sprites, frames):
+        super().__init__(frames, pos, groups)
+        self.flip = False
+        
+        # movement & collision
         self.direction = pygame.Vector2()
         self.collision_sprites = collision_sprites
         self.speed = 400
+        self.gravity = 50
+        self.on_floor = False
         
     def input(self):
         keys = pygame.key.get_pressed()
         self.direction.x = int(keys[pygame.K_d] or keys[pygame.K_RIGHT]) - int(keys[pygame.K_a] or keys[pygame.K_LEFT])
-        self.direction.y = int(keys[pygame.K_s] or keys[pygame.K_DOWN] - int(keys[pygame.K_w] or keys[pygame.K_UP]))
-        self.direction = self.direction.normalize() if self.direction else self.direction
+        if keys[pygame.K_SPACE] and self.on_floor:
+            self.direction.y = - 18
         
     def move(self, dt):
+        # horizonal
         self.rect.x += self.direction.x * self.speed * dt
         self.collision('horizontal')
-        self.rect.y += self.direction.y * self.speed * dt
+        
+        # vertical
+        self.direction.y += self.gravity * dt
+        self.rect.y += self.direction.y
         self.collision('vertical')
     
     def collision(self, direction):
@@ -37,9 +50,27 @@ class Player(Sprite):
                     if self.direction.x > 0: self.rect.right = sprite.rect.left
                     if self.direction.x < 0: self.rect.left = sprite.rect.right
                 else:
-                    if self.direction.y < 0: self.rect.top = sprite.rect.bottom
                     if self.direction.y > 0: self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: self.rect.top = sprite.rect.bottom
+                    self.direction.y = 0
+    
+    def check_floor(self):
+        bottom_rect = pygame.FRect((0, 0), (self.rect.width, 2)).move_to(midtop = self.rect.midbottom)
+        self.on_floor = True if bottom_rect.collidelist([sprite.rect for sprite in self.collision_sprites]) >= 0 else False
+    
+    def animate(self, dt):
+        if self.direction.x:
+            self.frame_index += self.animation_speed * dt
+            self.flip = self.direction.x < 0
+        else:
+            self.frame_index = 0
+        
+        self.frame_index = 1 if not self.on_floor else self.frame_index   
+        self.image = self.frames[int(self.frame_index) % len(self.frames)]
+        self.image = pygame.transform.flip(self.image, self.flip, False)
     
     def update(self, dt):
+        self.check_floor()
         self.input()
         self.move(dt)
+        self.animate(dt)
